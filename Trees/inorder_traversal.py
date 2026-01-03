bd0296c1c5faf0f0bdb2450f36938330c0dfe7b1
chaitanya teja <cg5501@srmist.edu.in>
2026-01-03 12:35:41 +0530

    Create inorder_traversal.py

---
+++ Trees/inorder_traversal.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.data, end=" ")
+        inorder(root.right)
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+inorder(root)
