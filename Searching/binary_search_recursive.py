d08228b8220656e01c394a9cd0eaa810e9809fb2
chaitanya teja <cg5501@srmist.edu.in>
2026-01-03 12:28:40 +0530

    Create binary_search_recursive.py

---
+++ Searching/binary_search_recursive.py
@@ -0,0 +1,15 @@
+def binary_search(arr, low, high, target):
+    if low > high:
+        return -1
+
+    mid = (low + high) // 2
+
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] > target:
+        return binary_search(arr, low, mid - 1, target)
+    else:
+        return binary_search(arr, mid + 1, high, target)
+
+arr = [10, 20, 30, 40, 50]
+print(binary_search(arr, 0, len(arr) - 1, 10))
