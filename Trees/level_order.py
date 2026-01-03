4579c44d3e3d8194492d1c27db5ea050e72ec179
chaitanya teja <cg5501@srmist.edu.in>
2026-01-03 12:36:37 +0530

    Create level_order.py

---
+++ Trees/level_order.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+def level_order(root):
+    if not root:
+        return
+    q = deque([root])
+    while q:
+        node = q.popleft()
+        print(node.data, end=" ")
+        if node.left:
+            q.append(node.left)
+        if node.right:
+            q.append(node.right)
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+level_order(root)
