a03471b5033db857cea939ef8f97b30be64102c6
chaitanya teja <cg5501@srmist.edu.in>
2026-01-04 12:08:20 +0530

    Create dfs.py

---
+++ Graphs/dfs.py
@@ -0,0 +1,16 @@
+def dfs(graph, node, visited):
+    if node not in visited:
+        print(node, end=" ")
+        visited.add(node)
+        for neighbor in graph[node]:
+            dfs(graph, neighbor, visited)
+
+graph = {
+    0: [1, 2],
+    1: [0, 3],
+    2: [0],
+    3: [1]
+}
+
+visited = set()
+dfs(graph, 0, visited)
