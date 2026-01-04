43064976dd400bf06d052d1a94b814fc6f34a003
chaitanya teja <cg5501@srmist.edu.in>
2026-01-04 12:05:30 +0530

    Create tree_height.py

---
+++ Trees/tree_height.py
@@ -0,0 +1,12 @@
+def height(root):
+    if not root:
+        return 0
+    return 1 + max(height(root.left), height(root.right))
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print("Height:", height(root))
